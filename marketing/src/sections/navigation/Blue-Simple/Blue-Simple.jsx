b4246680383627eff24cddba73e9bb3740a82e28
Mehrab <mehrab2000alam@gmail.com>
2023-03-21 15:56:51 -0400

    completed the BlueSimple navigation clone and made the changes as requested by reviewer

--- marketing/src/sections/navigation/Blue-Simple/Blue-Simple.jsx
+++ marketing/src/sections/navigation/Blue-Simple/Blue-Simple.jsx
@@ -1,6 +1,6 @@
 import "./Blue-Simple.css"
 import React from "react"
-import image from "../../../img/sections/hero/file-react.svg"
+
 
 
 const Feature = () => (
@@ -9,35 +9,35 @@ const Feature = () => (
 
             <ul className="features-option-item">
                 <li >
-                    <div><img src={image} alt="" /></div>
+                    <div><img src="img/sections/navigations/BlueSimple/device.svg" alt="" /></div>
                     <div>
                         <div>Sync across device</div>
                         <p>Enables to keep synchronized</p>
                     </div>
                 </li>
                 <li>
-                    <div><img src={image} alt="" /></div>
+                    <div><img src="img/sections/navigations/BlueSimple/folder.svg" alt="" /></div>
                     <div>
                         <div>Create fancy documents <span>&rarr;</span> </div>
                         <p>Makes your documents styled </p>
                     </div>
                 </li>
                 <li>
-                    <div><img src={image} alt="" /></div>
+                    <div><img src="img/sections/navigations/BlueSimple/pen.svg" alt="" /></div>
                     <div>
                         <div>Edit like a pro</div>
                         <p>Edit content is yours</p>
                     </div>
                 </li>
                 <li>
-                    <div><img src={image} alt="" /></div>
+                    <div><img src="img/sections/navigations/BlueSimple/file.svg" alt="" /></div>
                     <div>
                         <div>Organize documents</div>
                         <p>Keep it all in one place</p>
                     </div>
                 </li>
                 <li>
-                    <div><img src={image} alt="" /></div>
+                    <div><img src="img/sections/navigations/BlueSimple/manage.svg" alt="" /></div>
                     <div>
                         <div>Manage tasks and projects</div>
                         <p>Make simple checklists</p>
@@ -90,15 +90,16 @@ const BlueSimple = () => {
     return (
         <div className="blue-simple">
             <nav className="blue-simple-nav">
-                <img src={image} alt="" />
-                <a href="" className="features">
+                <img src="img/sections/navigations/BlueSimple/file.svg" alt="" />
+                <a href="">
                     <div onClick={(e) => {
                         e.preventDefault();
                         setOpen(!isOpen)
                     }}>
-                        <span>Features</span>
-                        {isOpen && (<Feature />)}
+                        <div className={isOpen ? "feature-open" : "feature-close"}>Features</div>
                     </div>
+                    {isOpen && (<Feature />)}
+
                 </a>
                 <a href="" >Downloads</a>
                 <a href="" >Resources</a>
