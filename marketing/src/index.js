df8a4d3e2c30daccdfcc14a5065f89d84b21ef46
Mehrab <mehrab2000alam@gmail.com>
2023-03-25 16:22:54 -0400

    Created the BlueSimple responsive nagivation

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import BlueSimple from "./sections/navigation/BlueSimple/BlueSimple";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode>
+  <BlueSimple />
+</React.StrictMode>);
 
 
 // If you want to start measuring performance in your app, pass a function
