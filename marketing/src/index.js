d71efd6a4324fadbcf29ee8ccb542707dbf1f066
Mehrab <mehrab2000alam@gmail.com>
2023-03-27 11:13:05 -0400

    changes are done as per reviewer request

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -6,9 +6,7 @@ import BlueSimple from "./sections/navigation/BlueSimple/BlueSimple";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>
-  <BlueSimple />
-</React.StrictMode>);
+root.render(<React.StrictMode></React.StrictMode>);
 
 
 // If you want to start measuring performance in your app, pass a function
