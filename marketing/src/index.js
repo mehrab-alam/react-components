ca2145a68f041a6eec127ee29de43819a7d9d49c
Mehrab <mehrab2000alam@gmail.com>
2023-03-29 13:22:56 -0400

    Removed the changes from index.js

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import KongNav from "./sections/navigation/KongNav/KongNav";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <KongNav />
-    </React.StrictMode>
+    <React.StrictMode>   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
