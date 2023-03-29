852d826a024f743f62e02dc69a13b800f9446b2b
mehrab-alam <122259451+mehrab-alam@users.noreply.github.com>
2023-03-29 09:40:14 +0530

    Created the BlueSimple responsive nagivation (#24)
    * Created the BlueSimple responsive nagivation
    
    * changes are done as per reviewer request
    
    * removed the import of BlueSimple from index.js

--- marketing/src/sections/navigation/BlueSimple/BlueSimple.jsx
+++ marketing/src/sections/navigation/BlueSimple/BlueSimple.jsx
@@ -1,93 +1,43 @@
 import "./BlueSimple.css";
+import "./BlueSimpleResponsive.css"
 import React from "react";
-import image from "../../../img/sections/hero/file-react.svg";
+import { featureItem, browsFeature } from "./Features";
+
+
 
 const Feature = () => (
   <div className='features'>
     <div className='features-option'>
       <ul className='features-option-item'>
-        <li>
-          <div>
-            <img src={image} alt='' />
-          </div>
-          <div>
-            <div>Sync across device</div>
-            <p>Enables to keep synchronized</p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src={image} alt='' />
-          </div>
-          <div>
-            <div>
-              Create fancy documents <span>&rarr;</span>{" "}
-            </div>
-            <p>Makes your documents styled </p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src={image} alt='' />
-          </div>
-          <div>
-            <div>Edit like a pro</div>
-            <p>Edit content is yours</p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src={image} alt='' />
-          </div>
-          <div>
-            <div>Organize documents</div>
-            <p>Keep it all in one place</p>
-          </div>
-        </li>
-        <li>
-          <div>
-            <img src={image} alt='' />
-          </div>
-          <div>
-            <div>Manage tasks and projects</div>
-            <p>Make simple checklists</p>
-          </div>
-        </li>
+        {featureItem.map((t, i) => {
+          return (
+            <>
+              <li key={i}>
+                <div>
+                  <img src={t.img} alt='' />
+                </div>
+                <div>
+                  <div>{t.title}</div>
+                  <p>{t.description}</p>
+                </div>
+              </li>
+            </>
+          )
+        })}
       </ul>
+
       <ul>
-        <li>
-          <div> Browse by Features</div>
-        </li>
-        <li>
-          <div> Formated text & Markdown</div>
-        </li>
-        <li>
-          <div> Documents themes</div>
-        </li>
-        <li>
-          <div> Convert & Compile</div>
-        </li>
-        <li>
-          <div> Apple pencil</div>
-        </li>
-        <li>
-          <div> Flowcharts</div>
-        </li>
-        <li>
-          <div> Change Tracking</div>
-        </li>
-        <li>
-          <div>
-            {" "}
-            Math Typsetting <span className='typsetting-status'>New</span>
-          </div>
-        </li>
-        <li>
-          <div> HQ Typesetting</div>
-        </li>
+        {browsFeature.map((k, i) => {
+          return (
+            <>
+              <li key={i}>
+                <div>{k.title}</div>
+              </li>
+            </>
+          )
+        })}
       </ul>
     </div>
-
     <div className='feature-footer'>
       <div className='ready-started'>
         <div>Ready to get started?</div>
@@ -100,26 +50,71 @@ const Feature = () => (
 
 const BlueSimple = () => {
   const [isOpen, setOpen] = React.useState(false);
+  const [isHamburger, setHamburger] = React.useState(false);
+  const [isFeatureActive, setFeatueActive] = React.useState(false);
+
+
   return (
-    <div className='blue-simple'>
-      <nav className='blue-simple-nav'>
-        <img src={image} alt='' />
-        <a href='' className='features'>
-          <div
-            onClick={(e) => {
-              e.preventDefault();
-              setOpen(!isOpen);
-            }}>
-            <span>Features</span>
-            {isOpen && <Feature />}
+    <div className="blue-simple-container">
+      <div className='blue-simple'>
+        <nav className='blue-simple-nav'>
+          <img src={"./img/sections/navigations/BlueSimple/file-react.svg"} alt='' />
+          <a href='' className='features'>
+            <div
+              onClick={(e) => {
+                e.preventDefault();
+                setOpen(!isOpen);
+              }}>
+              <span className={`features-title ${isOpen ? "features-menu-active" : "features-menu-inactive"}`}>Features</span>
+              <div className="feature-menu"> {isOpen && <Feature />}</div>
+            </div>
+
+          </a>
+          <a href=''>Downloads</a>
+          <a href=''>Resources</a>
+          <a href=''>Company</a>
+        </nav>
+      </div>
+      <div className="responsive-blue-simple">
+        <nav className="responsive-blue-simple-nav">
+          <img src="./img/sections/navigations/BlueSimple/file-react.svg" alt="" />
+          <div className={`hamburger ${isHamburger ? "hamburger-open" : "hamburger-close"}`} onClick={() => {
+            if (isHamburger) {
+              setFeatueActive(false)
+            }
+            setHamburger(!isHamburger)
+          }}>
+            <div className="slice slice-1"></div>
+            <div className="slice slice-2"></div>
+            <div className="slice slice-3"></div>
           </div>
-        </a>
-        <a href=''>Downloads</a>
-        <a href=''>Resources</a>
-        <a href=''>Company</a>
-      </nav>
+        </nav>
+        <div className={`sub-nav-list-menu ${isHamburger ? "sub-nav-open" : "sub-nav-close"}`}>
+          <div onClick={() => {
+            setFeatueActive(!isFeatureActive)
+          }}>Features</div>
+          <div>{featureItem.map((t, i) => {
+            return (
+              <>
+                <div className={`sub-featur-list ${isFeatureActive ? "feature-active" : "feature-inactive"}`} key={i}>
+                  <img src={t.img} alt="" />
+                  <div>
+                    <div>{t.title}</div>
+                    <div>{t.description}</div>
+                  </div>
+                </div>
+              </>
+            )
+          })}</div>
+          <div>Downloads</div>
+          <div>Resources</div>
+          <div>Company</div>
+
+        </div>
+      </div>
     </div>
   );
+
 };
 
 export default BlueSimple;
---
+++ marketing/src/sections/navigation/BlueSimple/Features.js
@@ -0,0 +1,46 @@
+const featureItem = [
+    {
+
+        img: "./img/sections/navigations/BlueSimple/file-react.svg",
+        title: "Sync across device",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        img: "./img/sections/navigations/BlueSimple/file-react.svg",
+        title: "  Create fancy documents",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        img: "./img/sections/navigations/BlueSimple/file-react.svg",
+        title: "Edit like a pro",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        img: "./img/sections/navigations/BlueSimple/file-react.svg",
+        title: "Organize documents",
+        description: "Lorem ipsum dolor sit amet."
+    },
+    {
+        img: "./img/sections/navigations/BlueSimple/file-react.svg",
+        title: "Manage tasks and projects",
+        description: "Lorem ipsum dolor sit amet."
+    },
+]
+
+const browsFeature = [
+    {
+        title: " Browse by Features"
+    },
+    { title: " Formated text & Markdown" },
+    { title: " Documents themes" },
+    { title: " Convert & Compile" },
+    { title: " Apple pencil" },
+    { title: " Flowcharts" },
+    { title: "  Change Tracking" },
+    { title: `   Math Typsetting ` },
+    { title: "  HQ Typesetting" },
+
+
+]
+
+export { featureItem, browsFeature }
