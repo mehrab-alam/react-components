ac0888a4a29d95d3ed93cfb6732a65d4b1a2f5d7
Mehrab <mehrab2000alam@gmail.com>
2023-03-29 10:37:32 -0400

    removed the import of BlueSimple from index.js

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import BlueSimple from "./sections/navigation/BlueSimple/BlueSimple";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
